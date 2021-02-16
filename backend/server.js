083c8d6ef0f6fc1ab2cc8f224e311c210c57a2b2
Dev-kris <61651282+Dev-kris@users.noreply.github.com>
2021-02-16 16:34:41 +0100

    Removed unnecessary readme file, added server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log('This is a test');
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("This is my server response");
+});
+
+server.listen(process.env.PORT || 3000);
