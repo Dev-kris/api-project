0d559aa46d0670851c550a603105c1b60b2821d1
Dev-kris <61651282+Dev-kris@users.noreply.github.com>
2021-02-17 17:16:12 +0100

    added test json response

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const http = require("http");
+//import app.js
+const app = require("./app");
 
-const server = http.createServer((req, res) => {
-  res.end("This is my second server response");
-});
+// set port for app
+app.set("port", process.env.PORT || 3000);
+//create server using app.js ( node creates server using express app)
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
