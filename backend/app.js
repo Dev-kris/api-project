cbea619a1e4e45e5bb68f4d69e9a42bc32f9019e
Dev-kris <61651282+Dev-kris@users.noreply.github.com>
2021-02-19 17:41:53 +0100

    added example middleware, improved server.js

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-  res.json({ message: "your request was successful." });
+app.use((req, res, next) => {
+  console.log("request received!");
+  next();
 });
 
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: "your request was successful." });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("Response sent successfully!");
+});
 //export app to allow access in other files
 module.exports = app;
